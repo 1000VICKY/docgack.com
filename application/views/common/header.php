1d0d441e13680a678e59793a765f0c8276de2266
1000VICKY <aki.sen.1209@gmail.com>
2016-04-08 07:53:07 +0900

    マスターブランチ
    マスターブランチと開発版ブランチの差分確認

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -33,4 +33,4 @@
                 <a href="/admin/index"><?php print($mainHeader); ?></a> | <a href="/admin/logout">ログアウト</a>
             </h2>
         </div><!--/#mainHeader -->
-        <p>git テスト</p>
+        <p>マスターブランチ差分確認</p>
